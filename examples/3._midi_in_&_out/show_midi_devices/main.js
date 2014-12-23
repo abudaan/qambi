8cdc3831084ce1d315ad505b127ee3eaa0dace1e
abudaan <daniel@abumarkub.net>
2014-12-23 14:11:34 +0100

    gh-pages

---
+++ examples/3._midi_in_&_out/show_midi_devices/main.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+
+    'use strict';
+
+    var
+        // satisfy jslint
+        sequencer = window.sequencer,
+        console = window.console,
+        output = document.getElementById('console');
+
+
+    sequencer.ready(function(){
+
+        if(sequencer.midi === false){
+            output.innerHTML = 'No MIDI I/O';
+            return;
+        }
+
+        // print all available midi inputs and outputs
+        output.innerHTML = 'MIDI in:<br/>';
+        sequencer.getMidiInputs(function(port){
+            //output.innerHTML += port.label + '<br/>';
+            output.innerHTML += port.name + ' ' + port.id + '<br/>';
+        });
+
+        output.innerHTML += '<br/>MIDI out:<br/>';
+        sequencer.getMidiOutputs(function(port){
+            //output.innerHTML += port.label + '<br/>';
+            output.innerHTML += port.name + ' ' + port.id + '<br/>';
+        });
+    });
+};
