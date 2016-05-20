8dfebda614f54fb1453600a2b90c7ca93fb8552b
abudaan <daniel@abumarkub.net>
2016-05-21 00:31:56 +0200

    wip

--- examples/midi-io/index.js
+++ examples/midi-io/index.js
@@ -16,7 +16,8 @@ document.addEventListener('DOMContentLoaded', function(){
   qambi.init({
     instrument: {
       type: 'Instrument',
-      url: 'http://qambi.org/instruments/heartbeat/city-piano.json',
+      url: 'https://abumarkub.net/qambi/instruments/heartbeat/city-piano.json',
+      //url: 'http://qambi.org/instruments/heartbeat/city-piano.json',
       //url: 'http://qambi.org/instruments/heartbeat/violin.json', // sustain loop
       //url: 'http://qambi.org/instruments/heartbeat/squareroot.json', // 2 velocity layers
     }
