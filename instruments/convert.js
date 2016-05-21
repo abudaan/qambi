5cb81d9cbad7870c66c7578bcfd4f83e2df750e3
abudaan <daniel@abumarkub.net>
2016-05-21 12:44:19 +0200

    moved instruments and samples back

---
+++ instruments/convert.js
@@ -0,0 +1,50 @@
+/*
+
+converts heartbeat instruments to qambi instrument files
+
+*/
+
+import fs from 'fs'
+
+let args = process.argv
+let json = args[2]
+let name = 'congas'
+//json = `/home/abudaan/workspace/heartbeat/assets/sso/strings/${name}.mp3.112.json` // violin
+json = `/home/abudaan/workspace/heartbeat/assets/sso/brass/${name}.mp3.112.json` // trumpet
+json = `/home/abudaan/workspace/heartbeat/assets/sso/percussion/${name}.mp3.112.json` // congas
+json = JSON.parse(fs.readFileSync(json))
+
+let instrument = json.instruments[0]
+let notes = instrument.mapping
+let samples = json.samplepacks[0].mapping
+let result = {
+  baseUrl: `https://raw.githubusercontent.com/abudaan/qambi-instruments/master/samples/${name}/`,
+  release: [instrument.release_duration, instrument.release_envelope]
+}
+
+console.log(json)
+
+for(let key of Object.keys(notes)){
+  let data = notes[key]
+  let sample = samples[data.n]
+  //sample.d -> data
+  console.log(key, data.n, sample.g, Object.keys(sample), data)
+
+  let sustain = samples[data.n].s
+
+  if(sustain && sustain.length === 2){
+    let sustainStart = sustain[0] / 1000 // convert to seconds
+    let sustainEnd = sustain[1] / 1000
+    result[key] = {
+      url: data.n + '.mp3',
+      sustain: [sustainStart, sustainEnd]
+    }
+  }else{
+    result[key] = {
+      url: data.n + '.mp3'
+    }
+  }
+}
+
+fs.writeFileSync(`./instruments/heartbeat/${name}.json`, JSON.stringify(result))
+
